2a1107211a9b14d95d1abc8801f966ea0742ad5f
flycloudc <flycloudc@qq.com>
2025-01-27 14:50:45 +0800

    refactor: 将代码移至单独的文件

--- notes/rend.js
+++ notes/rend.js
@@ -1,24 +1,33 @@
-// 获取body中所有的文本，将形如 "{xxx}"的都替换成"<span class='code'>xxx</span>"
+function code_span(code_or_ref_text) {
+    if (/\n/.test(code_or_ref_text)) {
+        console.error(code_or_ref_text)
+        return
+        throw new Error('Multiline code should use outer reference');
+    }
 
-function remove_all_prepending_spaces(lines) {
-    // 统计每行开头的空格数
-    const indents = lines.map(line => {
-        const match = line.match(/^\s*/);
-        return match ? match[0].length : 0;
-    });
-    // 找到最小的空白字符数
-    const min_indent = Math.min(...indents);
-    // 删除所有开头的空白字符
-    lines.forEach((line, i) => {
-        if (indents[i] > 0) {
-            lines[i] = line.slice(min_indent);
-        }
-    });
+    const span = document.createElement('span');
+    span.classList.add('code');
+    span.textContent = code_or_ref_text;
+    if (code_or_ref_text.startsWith('## ')) { // is reference
+        span.classList.add('multiline');
+        // 等待异步加载代码
+    }
+    return span;
 }
 
-function lines_to_nodes(lines) {
-    const length = lines.length;
-    return lines.flatMap((line, i) => {
+/**
+ * @param {string} code_text 
+ */
+function multiline_code_to_nodes(code_text) {
+    // 如果有换行，先拆分成每行，删除首尾空白行
+    const lines = code_text.split('\n');
+    if (/^\s*$/.test(lines[0])) {
+        lines.shift();
+    }
+    if (/^\s*$/.test(lines[lines.length - 1])) {
+        lines.pop();
+    }
+    return lines.flatMap((line) => {
         const result = [];
         const indent = line.indexOf(';');
 
@@ -33,41 +42,13 @@ function lines_to_nodes(lines) {
             comment_node.textContent = comment + '\n';
             result.push(code_node, comment_node);
         }
-
-        if (i < length - 1) {
-            // result.push(document.createElement('br'));
-        }
         return result;
     })
 }
 
-function code_span(text) {
-    // 如果没有换行，直接返回一个span节点
-    if (!/\n/.test(text)) {
-        const span = document.createElement('span');
-        span.classList.add('code');
-        span.textContent = text;
-        return span;
-    } else {
-        // 如果有换行，先拆分成每行，删除首尾空白行
-        const lines = text.split('\n');
-        if (/^\s*$/.test(lines[0])) {
-            lines.shift();
-        }
-        if (/^\s*$/.test(lines[lines.length - 1])) {
-            lines.pop();
-        }
-        remove_all_prepending_spaces(lines);
-
-        const span = document.createElement('span');
-        span.classList.add('code');
-        span.classList.add('multiline');
-        span.append(...lines_to_nodes(lines));
-
-        return span;
-    }
-}
-
+/**
+ * @param {Node} node 
+ */
 function replace_text_to_code_span(node) {
     // 遍历每个子节点
     for (let i = 0; i < node.childNodes.length; i++) {
@@ -77,7 +58,7 @@ function replace_text_to_code_span(node) {
             replace_text_to_code_span(child);
         } else if (child.nodeType === Node.TEXT_NODE) {
             let need_replace = false;
-            const replaced = child.textContent.split(/(\{[^\}]+\})/).map(part => {
+            const replaced = child.textContent.split(/({.*?})/g).map(part => {
                 if (part.startsWith('{') && part.endsWith('}')) {
                     need_replace = true;
                     return code_span(part.slice(1, -1));
@@ -96,6 +77,54 @@ function replace_text_to_code_span(node) {
     }
 }
 
+/**
+ * @param {string} url 
+ */
+async function fetch_code_example(url) {
+    const response = await fetch(url);
+    const text = await response.text();
+    const codes = text.split('---');
+    if (codes.length > 0 && codes[codes.length - 1].trim() === '') {
+        codes.pop();
+    }
+    const map = new Map(codes.map(title_and_code => {
+        title_and_code = title_and_code.trim();
+        // 找到第一个换行符
+        const index = title_and_code.indexOf('\n');
+        if (index === -1) {
+            throw new Error(`Invalid code block: ${title_and_code}`);
+        }
+        const title = title_and_code.slice(0, index);
+        const code = title_and_code.slice(index + 1);
+        return [title.trim(), code.trim()];
+    }));
+    return map;
+}
+
+
+/**
+ * @param {Map<string, string>} code_map 
+ */
+function load_example(code_map) {
+    // 获取所有的<span>节点
+    const code_nodes = document.querySelectorAll('span');
+    // 如果它的内容是"## xxx"，则替换成对应的代码
+    code_nodes.forEach(node => {
+        const text = node.textContent.trim();
+        if (text.startsWith('## ')) {
+            const title = text.slice(3);
+            const code = code_map.get(title);
+            if (code) {
+                node.removeChild(node.firstChild);
+                node.append(...multiline_code_to_nodes(code));
+            } else {
+                throw new Error(`Code not found: ${title}`);
+            }
+        }
+    });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     replace_text_to_code_span(document.body);
+    fetch_code_example('example.scm').then(load_example)
 });
